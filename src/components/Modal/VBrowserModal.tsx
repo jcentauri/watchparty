648c9dc030c019d0513c5687e19cde4cedd25fd8
jcentauri <59785979+jcentauri@users.noreply.github.com>
2021-09-06 16:37:44 +0800

    Update VBrowserModal.tsx

--- src/components/Modal/VBrowserModal.tsx
+++ src/components/Modal/VBrowserModal.tsx
@@ -27,6 +27,7 @@ export class VBrowserModal extends React.Component<{
     const LaunchButton = withGoogleReCaptcha(
       ({ googleReCaptchaProps, large }: any) => (
         <Button
+          display='none"
           size="large"
           color={large ? 'orange' : undefined}
           onClick={async () => {
